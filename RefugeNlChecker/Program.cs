45cf525531b68370a556a41a2024719d5f0f924c
Ivan Derevianko <drussilla7@gmail.com>
2022-08-04 10:54:31 +0200

    Get token automatically

--- RefugeNlChecker/Program.cs
+++ RefugeNlChecker/Program.cs
@@ -4,11 +4,15 @@ using RefugeNlChecker;
 
 Console.WriteLine("Start https://portaal.refugeepass.nl/en/make-an-appointment");
 
-using var handler = new HttpClientHandler {UseCookies = false};
+using var handler = new HttpClientHandler { UseCookies = false };
+using var client = new HttpClient(handler);
 
-using var client = new HttpClient(handler) { BaseAddress = new Uri("https://post.refugeepass.nl") };
+List<string> cookies = new List<string>();
+string token = string.Empty;
+await GetNewToken();
 
 using var telegramClient = new HttpClient();
+var telegramChatId = Environment.GetEnvironmentVariable("TG_ChatId");
 var telegramBotSecret = Environment.GetEnvironmentVariable("TG_Secret");
 
 await SendMessage("Start!%20https://portaal.refugeepass.nl/en/make-an-appointment");
@@ -21,9 +25,9 @@ try
 
         while (date < new DateTime(2022, 10, 01))
         {
-            if (await CheckEndpoint(date, "/api/v1/appointment/get-available-options")) return;
+            if (await CheckEndpoint(date, "https://post.refugeepass.nl/api/v1/appointment/get-available-options")) return;
             Thread.Sleep(TimeSpan.FromSeconds(1));
-            if (await CheckEndpoint(date, "/api/v1/appointment/get-alternative-options")) return;
+            if (await CheckEndpoint(date, "https://post.refugeepass.nl/api/v1/appointment/get-alternative-options")) return;
 
             date = date.AddDays(1);
             Thread.Sleep(TimeSpan.FromSeconds(1));
@@ -39,9 +43,34 @@ catch (Exception ex)
     throw;
 }
 
+async Task GetNewToken()
+{
+    Console.WriteLine("Getting new token");
+    var response = await client.GetAsync("https://portaal.refugeepass.nl/en/make-an-appointment");
+    response.EnsureSuccessStatusCode();
+    cookies = response.Headers.GetValues("set-cookie").ToList();
+    token = ExtractCookie(cookies.First(x => x.StartsWith("XSRF-TOKEN"))).Value.Replace("%3D", "=");
+}
+
+(string Name, string Value) ExtractCookie(string setCookieString)
+{
+    var nameValueSplit = setCookieString.IndexOf("=", StringComparison.InvariantCultureIgnoreCase);
+    var cookieEnd = setCookieString.IndexOf(";", StringComparison.InvariantCultureIgnoreCase);
+
+    if (nameValueSplit == -1 || cookieEnd == -1)
+    {
+        throw new FormatException($"Wrong cookie format. Expected <name>=<value>; but got: {setCookieString}");
+    }
+
+    var name = setCookieString.Substring(0, nameValueSplit);
+    var value = setCookieString.Substring(nameValueSplit + 1, cookieEnd - nameValueSplit - 1);
+
+    return (name, value);
+}
+
 Task SendMessage(string message)
 {
-    return telegramClient.GetAsync($"https://api.telegram.org/bot{telegramBotSecret}/sendMessage?chat_id=423630263&text={message}");
+    return telegramClient.GetAsync($"https://api.telegram.org/bot{telegramBotSecret}/sendMessage?chat_id={telegramChatId}&text={message}");
 }
 
 async Task<bool> CheckEndpoint(DateTime selectedDate, string url)
@@ -61,12 +90,8 @@ async Task<bool> CheckEndpoint(DateTime selectedDate, string url)
             info = new() { telephone_number = string.Empty }
         };
 
-        var token =
-            "eyJpdiI6InFVVTZrM2hXNEU5ejhGeFdMNEozL0E9PSIsInZhbHVlIjoiZmdJa25ZQ2g2RjhEbEdKYW5XZVI1N0hROGlwdGVTdUNsMFhqVit1SERNK1FOTDVFWCthbUl5TEJxRGFxSjlCYXQ2OHhLc1NqR3NabmFuSmpTNXdvSDI1a2lUT203bDdyQzdUZ3E3YThUZmpLQzlyUWhHSW03ZzNHeVdrMWZ0aTgiLCJtYWMiOiI1MmEyNjEwNGUxODAyMzExMDY0YTQxYmZmMWIwZTkzMGMyN2RhYWI0Yjk0N2U4NTVjNGFjMGM3ZTVkNjIyZjhlIiwidGFnIjoiIn0=";
-
         var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, endpoint);
-        httpRequestMessage.Headers.Add("Cookie",
-            "XSRF-TOKEN=eyJpdiI6InFVVTZrM2hXNEU5ejhGeFdMNEozL0E9PSIsInZhbHVlIjoiZmdJa25ZQ2g2RjhEbEdKYW5XZVI1N0hROGlwdGVTdUNsMFhqVit1SERNK1FOTDVFWCthbUl5TEJxRGFxSjlCYXQ2OHhLc1NqR3NabmFuSmpTNXdvSDI1a2lUT203bDdyQzdUZ3E3YThUZmpLQzlyUWhHSW03ZzNHeVdrMWZ0aTgiLCJtYWMiOiI1MmEyNjEwNGUxODAyMzExMDY0YTQxYmZmMWIwZTkzMGMyN2RhYWI0Yjk0N2U4NTVjNGFjMGM3ZTVkNjIyZjhlIiwidGFnIjoiIn0%3D; refugee_pass_session=eyJpdiI6IkhYdXBSeU0wZXZPbEtIV011dm45dFE9PSIsInZhbHVlIjoiTDZKYWtrbnRYbmZuaVN3VStsNFl4ZDY4SXNmbU8xNFBJczdBdlZHeVJPNm5QWDR5K0tuVWpha25KL2pmZ0lQcVczWW5zS0NTU2kvek9wdlNqTmZ6MlIzazd4TjRVZExBMWdTV2xsa3lCclJCSEJsOFp5NitLL25WUkJRLy9oN00iLCJtYWMiOiJmNWEwZWJmNDgyMDk1YjViZTY1OTI1MzZmNmU1OWJhYjZjNThiYTM2ZmZiNjllMTZlYjY5MjZiNjkxNjgwN2JlIiwidGFnIjoiIn0%3D; locale=eyJpdiI6IkVYUnZ0aUJsd3R4WTQrOXN4TTcwWVE9PSIsInZhbHVlIjoiT0l2Vjl4L1NKcEdIYzZEdjBWQXFxQkpOL0RFN1k0U2NpaTlvUmtnQ2dlVG9xVDBnSVpFTng5WS9uYkQ2aXdULyIsIm1hYyI6IjEwYzgzZTc3ZmQwOTliMWI4ODYzZTk4MTVkMDhmZGEwOThjNjMwOTg1NDVhODI1OThlOTIwMDIxMjJhMWMzODciLCJ0YWciOiIifQ%3D%3D; A0ceNtNtHhd6nhGefTG6FVzwh1xrxZm9TFKo0CWR=eyJpdiI6Ii9rd2V1YTNYL2oyUHFubkNVSFFxNlE9PSIsInZhbHVlIjoieUlsblZiVDZYam5Md2NhUEhFV2Qxczhudll4VVpUaC9EaE9PV2xVVnhkSTZGTHNCZzh0Y2NhcFowVjc4YUNjcDRPRjBTY3AwaDJnSjFZRFY0azQ4QmN3MEMraVpWNzlCY1lqaWtvQ3d6VlMwTVpxZVo2VFRKdU1Oa0U1RHRZVEh1TXpLOVhFOG9kSmhUQkZmeGIwbE9ZdE9DNjJhSWR5ZmpGTjBzNGZGOW54YUtsTE96T24vS00wUUN2U01xQXlQd2l5LzlwQ1VUTTBMSFBPV2VGVUJSQkxrcmkxY09qcmlQRXJ4MFBtTkdxUE1pek1KT1FscTdjQ3dOZmNDaWltQXk4aTNmQ042S3pDbVpvWHRBblJiZVM4VlFGTXRyQVoyY1JKQVdnT0tNT0Rzem1KRy9Vd3ZRZ0M2ckVSVXdRRUxqeFF5YWY4QWp0T1NITXdiS3ZPbW41aERVblZKWXdwVWVpL3FjUTJ4TU5wVlErMzR3N1BiUGo2MkJuYmJ1K2c5eEl4TVlNdVZNODAxRHYzZTJYQmxCVS84S3FMVUYzVTgvdko2NGo2cE5vZEIvYTV3QzhCeUVSL0F6L3VLVzFrMCIsIm1hYyI6ImY1ZDM3MzZiNWJkYzk1ZDNmMzIyM2E5OTQ4NGMzNDkwMWNjNjA5NDczYTFiOTNhMGZmMzczOTc2ZTQ3ZDU2NTkiLCJ0YWciOiIifQ%3D%3D");
+        httpRequestMessage.Headers.Add("Cookie", string.Join("; ", cookies));
         httpRequestMessage.Headers.Add("x-xsrf-token", token);
         httpRequestMessage.Content = JsonContent.Create(data);
         return httpRequestMessage;
@@ -77,7 +102,9 @@ async Task<bool> CheckEndpoint(DateTime selectedDate, string url)
     var resp = await client.SendAsync(request);
     if ((int)resp.StatusCode == 419)
     {
-        throw new Exception("Token expired!");
+        Console.WriteLine("Token expired!");
+        await GetNewToken();
+        return false;
     }
     
     if (resp.StatusCode != HttpStatusCode.OK)
@@ -99,7 +126,7 @@ async Task<bool> CheckEndpoint(DateTime selectedDate, string url)
         return true;
     }
 
-    Console.WriteLine($"{selectedDate} nothing :( rate left: {rateLeft}");
+    Console.WriteLine($"{selectedDate:yyyy-MM-dd} nothing :( rate left: {rateLeft}");
     return false;
 }
 
